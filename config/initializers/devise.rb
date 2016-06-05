b98953314720110e4bccf9a70673167ec5202b97
Liz Bayardelle <lizbayardelle@gmail.com>
2016-06-05 10:18:50 -0700

    Mailer Fixes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,14 +10,14 @@ Devise.setup do |config|
 
   # STUFF FOR DEVISE
   config.allow_unconfirmed_access_for = 365.days
-  
+
 
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'blocitoff@example.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
