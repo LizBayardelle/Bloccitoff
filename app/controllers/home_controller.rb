aa585f95f7c8ee9fb449fb4f83342c42d082be34
Liz Bayardelle <lizbayardelle@gmail.com>
2016-06-14 17:33:13 -0700

    Things Fixed, AJAXically

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,12 +5,14 @@ class HomeController < ApplicationController
     @items = Item.all
     if current_user
         @my_lists = current_user.lists
-        @shared_lists = @lists.where(:user_id != current_user.id && :shared_with == current_user.email)
+        @lists_not_by_me = @lists.where.not(user_id: current_user)
+        @shared_lists = @lists_not_by_me.where(shared_with: current_user.email)
 
         @my_items = current_user.items
         @top_items = @my_items.limit(4).order(created_at: :desc)
         @items_delegated_by = @my_items.where.not(delegated_to: "")
-        @items_delegated_to = @items.where(:delegated_to == current_user.email)
+        @items_not_by_me = @items.where.not(user_id: current_user)
+        @items_delegated_to = @items_not_by_me.where(:delegated_to == current_user.email)
         @top_items_delegated_by = @items_delegated_by.limit(4)
         @top_items_delegated_to = @items_delegated_to.limit(4)
     end
